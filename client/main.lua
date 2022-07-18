311840b354a12da95f822a85cb4395c9a1e7d1fb
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-07-19 01:10:09 +0200

    Add files via upload

---
+++ client/main.lua
@@ -0,0 +1,28 @@
+function ShowText(data)
+    print("yes")
+    SendNUIMessage({
+        action = "updatevisibility",
+        value = true
+    })
+    SendNUIMessage({
+        action = "updatedata",
+        data = data
+    })
+end
+exports("ShowText", ShowText)
+
+function UpdateText(data)
+    SendNUIMessage({
+        action = "updatedata",
+        data = data
+    })
+end
+exports("UpdateText", UpdateText)
+
+function HideText()
+    SendNUIMessage({
+        action = "updatevisibility",
+        value = false
+    })
+end
+exports("HideText", HideText)
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+version '1.0.0'
+author "Zerio#0880"
+description "Beautiful and easy to use activity text"
+
+escrow_ignore {"client/*.lua"}
+
+client_scripts {"client/main.lua"}
+
+files {"html/libs/*.js", "html/script.js", "html/style.css", "html/index.html"}
+
+ui_page "html/index.html"
